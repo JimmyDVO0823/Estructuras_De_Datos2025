28b25af1c09abdef7ca0a74028af365de8f07f9f
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-02-19 09:27:41 -0500

    Añadí algunos de los métodos mas comunes

--- ListasAnidadas/src/main/java/EstructurasImplementadas/ListaAnidada.java
+++ ListasAnidadas/src/main/java/EstructurasImplementadas/ListaAnidada.java
@@ -2,14 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package EstructurasImplementadas;
+package ListaEnlazadaSimple;
 
 /**
  *
  * @author LENOVO LOQ
  */
-public class ListaAnidada {
-    Nodo cabeza;
+public class LinkedList {
+    Nodo head;
+    Nodo tial;
     
     static class Nodo{
         int data;
@@ -22,20 +23,64 @@ public class ListaAnidada {
     }
     
     public void aniadirFinal(Nodo ultimo){
-        Nodo n = cabeza;
         
-        if(n == null){
-        cabeza = ultimo;
+        aniadirVacio(ultimo);
+        if (tial != null){
+            tial.next = ultimo;
+            tial = ultimo;
         }
-        else{
-            while(n.next != null){
-                n = n.next;
+    }
+    
+    public void aniadirInicio(Nodo primero){
+        aniadirVacio(primero);
+        if (tial != null){
+            primero.next = head;
+            head = primero;
+        }
+    }
+    
+    public void aniadirEnPosicion(int posicion, Nodo nuevo){
+        Nodo nodoActual = head;
+        Nodo nodoSiguienteActual;
+        
+        
+        aniadirVacio(nuevo);
+        for (int i = 0; i <= posicion; i++) {
+            if(i == posicion-1){
+                nodoSiguienteActual = nodoActual.next;
+                nuevo.next = nodoSiguienteActual;
+                nodoActual.next = nuevo;
             }
-            n.next = ultimo;
+            
+            nodoActual = nodoActual.next;
         }
-        
         
     }
     
+    public void eliminarInicio(){
+        if(head != null) head = head.next;
+    }
+    
+    public void eliminarFinal(){
+        Nodo nodoActual = head;
+        boolean continuarBucle = true;
+        if(tial != null){
+            while(continuarBucle){
+                if(nodoActual.next.next == null){
+                    tial = nodoActual.next;
+                    continuarBucle = false;
+                }
+            }
+        }
+    }
+    
+    
+    
+    public void aniadirVacio(Nodo nuevoNodo){
+        if(tial == null){
+        head = nuevoNodo;
+        tial = nuevoNodo;
+        }
+    }
     
 }
